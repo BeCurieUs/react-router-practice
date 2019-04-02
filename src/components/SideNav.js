3c44b28306d22279f83fed74172bb9dd223a3b87
Christopher Willis <willis.christopher.a@gmail.com>
2019-04-02 17:59:29 -0500

    main changes

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,26 +1,29 @@
 import React from "react";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function SideNav() {
   return (
     <div className="collapse navbar-collapse navbar-ex1-collapse">
       <ul className="nav navbar-nav side-nav">
         <li className="active">
-          {/*
-          <Link to="/"> <i className="fa fa-fw fa-dashboard" />
+          
+          <Link to="/"> 
+          <i className="fa fa-fw fa-dashboard" />
             Dashboard 
           </Link>
-          */}
+         
         </li>
         <li>
-          <a href="charts.html">
-            <i className="fa fa-fw fa-bar-chart-o" /> Charts
-          </a>
+          <Link to="/charts">
+            <i className="fa fa-fw fa-bar-chart-o" /> 
+            Charts
+          </Link>
         </li>
         <li>
-          <a href="tables.html">
-            <i className="fa fa-fw fa-table" /> Tables
-          </a>
+          <Link to="/tables">
+            <i className="fa fa-fw fa-table" /> 
+            Tables
+          </Link>
         </li>
       </ul>
     </div>);  
